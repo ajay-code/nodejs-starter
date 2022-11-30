9835fd6b63a7f730b6c3700024161019cd6cf90d
Ajay Singh <ajay10mar96@gmail.com>
2022-11-30 10:18:06 +0530

    adding knexjs as session data store

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,16 @@ import path from "path";
 import config from "#src/config/index.js";
 import express from "express";
 import session from "express-session";
+import knexSession from "connect-session-knex";
 import * as middleware from "#src/middleware/index.js";
 import routes from "#src/routes/index.js";
 import { getDirname } from "./utils/index.js";
 import passport from "./auth/passport.js";
+import db from "./db/index.js";
 
 const __dirname = getDirname(import.meta.url);
 const app = express();
+const knexSessionStore = knexSession(session);
 
 app.use(express.static(path.resolve(__dirname, "../public")));
 app.set("views", path.resolve(__dirname, "../views"));
@@ -20,13 +23,19 @@ app.use(express.json()); // parse json
 app.use(express.urlencoded()); // parse form data
 
 // initialize session middleware
-app.use(
-  session({
-    secret: config.APP_KEY,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+const sessionOpt = {
+  secret: config.APP_KEY,
+  resave: false,
+  saveUninitialized: true,
+  store: new knexSessionStore({
+    knex: db,
+    tablename: "sessions",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60,
+  }),
+};
+app.use(session(sessionOpt));
+
 app.use(passport.initialize());
 app.use(passport.session());
 // add routes to the app
