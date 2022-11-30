c545e6ca86e3ec5b37f99ffd58cfa7ab3e8552c0
Ajay Singh <ajay10mar96@gmail.com>
2022-11-30 08:35:04 +0530

    formatting ejs files

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import "express-async-errors";
 import path from "path";
 import config from "#src/config/index.js";
 import express from "express";
-import session from "express-session"
+import session from "express-session";
 import * as middleware from "#src/middleware/index.js";
 import routes from "#src/routes/index.js";
 import { getDirname } from "./utils/index.js";
@@ -16,21 +16,23 @@ app.set("views", path.resolve(__dirname, "../views"));
 app.set("view engine", "ejs");
 
 // parse request data
-app.use(express.json()) // parse json
-app.use(express.urlencoded()) // parse formdata
+app.use(express.json()); // parse json
+app.use(express.urlencoded()); // parse form data
 
 // initialize session middleware
-app.use(session({
-  secret: config.APP_KEY,
-  resave: false,
-  saveUninitialized: true,
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(
+  session({
+    secret: config.APP_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 // add routes to the app
 routes(app);
 
 // 404 middleware
 app.use(middleware.notFound);
 app.use(middleware.errorHandler);
-export default app
+export default app;
