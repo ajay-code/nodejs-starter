3fc302e135ad1f8b2f5a474536dd18007ff5d462
Ajay Singh <ajay10mar96@gmail.com>
2022-12-19 10:59:56 +0530

    adding app key generation script

--- src/db/seeds/users.ts
+++ src/db/seeds/users.ts
@@ -1,4 +1,5 @@
 import { Knex } from "knex";
+import passwordService from "#src/services/password.service.js";
 
 /**
  * @param { import("knex").Knex } knex
@@ -7,8 +8,9 @@ import { Knex } from "knex";
 export const seed = async function (knex: Knex) {
   // Deletes ALL existing entries
   await knex('users').del()
+  const password = await passwordService.hash('secret')
   await knex('users').insert([
-    { id: 1, email: 'ajay@email.com', name: "Ajay Singh", password: "secret" },
+    { id: 1, email: 'ajay@email.com', name: "Ajay Singh", password }
   ]);
 };
 
