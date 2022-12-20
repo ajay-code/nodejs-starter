7d8e8d06bbd3a3a749953eae3cf816bffd811cb1
Ajay Singh <ajay10mar96@gmail.com>
2022-12-20 12:40:49 +0530

    removing commented import file

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,37 @@
-// import config from "#src/config/index.js";
 import path from "node:path";
 import express from "express";
 import helmet from "helmet"
 import * as middleware from "#src/middleware/index.middleware.js";
 import addRoutes from "#src/routes/index.routes.js";
-import { getDirname } from "./utils/index.js";
+import { getDirname } from "#src/utils/index.js";
 
 const __dirname = getDirname(import.meta.url);
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "../public")));
 app.set("views", path.resolve(__dirname, "../views"));
 app.set("view engine", "ejs");
+app.disable('x-powered-by')
 
 /**
  * parse request data
- */
+*/
 app.use(express.json()); // parse json
 app.use(express.urlencoded({ extended: false })); // parse form data
 
 
 /**
- * add security middlewares
+ * add security middleware
  */
 app.use(helmet())
 
 /**
- * add routes to the app
+ * Serve static files form public dir
  */
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+/**
+ * add routes to the app
+*/
 addRoutes(app);
 
 /**
