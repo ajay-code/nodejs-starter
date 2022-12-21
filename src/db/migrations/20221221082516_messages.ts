8effedc1399866f74b3d91795ea19ebf521913c8
Ajay Singh <ajay10mar96@gmail.com>
2022-12-21 15:41:49 +0530

    adding new table migration

---
+++ src/db/migrations/20221221082516_messages.ts
@@ -0,0 +1,27 @@
+import { Knex } from "knex";
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const up = function (knex: Knex) {
+    return knex.schema.createTable("messages", (table) => {
+        table.increments("id")
+        table.text("message").notNullable()
+        table.integer("user_id")
+            .unsigned()
+            .notNullable()
+            .references("id")
+            .inTable("users")
+            .onDelete("CASCADE")
+        table.timestamp("created_at").defaultTo(knex.raw("CURRENT_TIMESTAMP"))
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export const down = function (knex: Knex) {
+    return knex.schema.dropTable("messages")
+};
