b9236465ddc8205fefff09cb065d5e99aa5beabc
Ajay Singh <ajay10mar96@gmail.com>
2022-12-24 15:00:28 +0530

    refactoring script upgradeing jsownwebtoke  to v9.0.0

--- src/scripts/genKey.ts
+++ src/scripts/genKey.ts
@@ -1,32 +1,11 @@
 #! /usr/bin/env node
-import * as fs from "node:fs"
 import { randomBytes } from "node:crypto"
+import updateEnvFile from "./updateEnvFile.js"
 
 function genKey(): string {
     return randomBytes(32).toString('hex')
 }
 
-function updateEnv(key: string, value: string) {
-    const data = fs.readFileSync('.env', 'utf-8')
-    const lines = data.split('\n')
-    let found = false
-
-    const newLines = lines.map(line => {
-        if (line.startsWith(`${key}=`)) {
-            found = true;
-            return `${key}=${value}`
-        } else {
-            return line
-        }
-    })
-
-    if (!found) {
-        newLines.push(`${key}=${value}`)
-    }
-
-    const newData = newLines.join('\n')
-    fs.writeFileSync(".env", newData)
-}
-
 const key = genKey()
-updateEnv("APP_KEY", key)
+
+updateEnvFile("APP_KEY", key)
