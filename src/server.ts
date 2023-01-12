60c995092106a23c5d71f09e7dcb71013b5ba09c
Ajay Singh <ajay10mar96@gmail.com>
2023-01-12 14:19:48 +0530

    adding handlers for uncaughtException and beforeExit events

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import config from '#src/config/index.config.js'
 import 'express-async-errors'
 import http from 'node:http'
 import app from './app.js'
+import db from './lib/knex/db.js'
 
 const port = config.APP_PORT
 const baseUrl = config.BASE_URL
@@ -11,4 +12,14 @@ const server = http.createServer(app)
 
 server.listen(port, () => {
     console.log(`The website is running on ${baseUrl}:${port}`)
+})
+
+process.on('uncaughtException', (err) => {
+    console.error(new Date().toUTCString() + ' uncaughtException:', err.message)
+    console.error(err.stack)
+    process.exit(1)
+})
+
+process.on('beforeExit', (code) => {
+    db.destroy()
 })
